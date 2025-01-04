89ec99ac9b7b91d2f77fb4d7ba95a99c62a714f9
Murat Can <muratcan.eravsar@hotmail.com>
2025-01-04 23:24:59 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# era1-api-task
+
+-Hello, the purpose of the project is to create a node js project with clean architecture.
+
+# Setup
+
+-Use the relevant code directory when you first opened the project. Making packages for installation within the project.
+
+`$ npm install`
+
+-Then use the relevant code directory for the project to work.
+
+`$ npm start`
---
+++ index.js
@@ -0,0 +1,19 @@
+const bootstrap = require('./lib/infrastructure/config/bootstrap');
+const createServer = require('./lib/infrastructure/webserver/server');
+
+// Start the server
+const start = async () => {
+
+    try {
+        await bootstrap.init();
+        await createServer();
+
+
+
+    } catch (err) {
+        console.log(err);
+        process.exit(1);
+    }
+};
+
+start();
